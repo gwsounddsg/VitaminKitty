de8027b4444f62889c539e1a520889fae1140926
GW Rodriguez <gwsounddsg@gmail.com>
2020-04-06 09:47:52 -0400

    adds Facts, updates using's in CatFact

---
+++ VitaminKitty/Models/Fact.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitaminKitty.Models
+{
+    public class Fact
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        [JsonProperty("type")]
+        public string Type { get; set; }
+        [JsonProperty("upvotes")]
+        public int UpVotes { get; set; }
+    }
+}
--- VitaminKitty/Web/CatFact.cs
+++ VitaminKitty/Web/CatFact.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using VitaminKitty.Models;
 
 namespace VitaminKitty.Web
 {
