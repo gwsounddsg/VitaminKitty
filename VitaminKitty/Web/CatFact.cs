6957b7413a9c951dcdec0fc1982d72458fdd5774
GW Rodriguez <gwsounddsg@gmail.com>
2020-04-06 09:42:29 -0400

    adds CatFact with interface, adds Models and Web folder to project

---
+++ VitaminKitty/Web/CatFact.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VitaminKitty.Web
+{
+    public class CatFact
+    {
+        private readonly string _url = "https://cat-fact.herokuapp.com/facts";
+
+        public string GetFact()
+        {
+            // calls url and converts result to json string
+            WebRequest request = WebRequest.Create(_url);
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+
+            //// gets first fact in array
+            //var facts = JsonConvert.DeserializeObject<dynamic>(content);
+            //return facts.all[0].text;
+
+            List<Fact> facts = JsonConvert.DeserializeObject<List<Fact>>(content);
+            Console.WriteLine(facts[0]);
+
+            return "";
+        }
+    }
+}
