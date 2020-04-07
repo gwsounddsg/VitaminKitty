002a7b01d2700644c7692c756732e6b772ec33b9
GW Rodriguez <gwsounddsg@gmail.com>
2020-04-07 14:02:29 -0400

    adds KittyImage tests

---
+++ VitaminKitty.UnitTests/KittyImageTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+using VitaminKitty.Services;
+
+namespace VitaminKitty.UnitTests
+{
+    class KittyImageTests
+    {
+        private KittyImage kittyImage;
+
+        [SetUp]
+        public void Setup()
+        {
+            kittyImage = new KittyImage();
+        }
+
+        [Test]
+        public void TestKittyImage_RandomKitty()
+        {
+            Bitmap image = kittyImage.RandomKitty();
+            Assert.That(image, Is.Not.Null);
+        }
+    }
+}
